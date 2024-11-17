e8d82ff5ebd9cdca3f47a374b4f16ae4e5993c1e
khadhijasalam <khadhijahameedsalam@gmail.com>
2024-11-17 19:45:57 +0530

    Update Contact form - Added labels and changed placeholders for SEO optimization

--- static/php/form-handler.php
+++ static/php/form-handler.php
@@ -1,26 +1,34 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+// Capture form data
+$name = htmlspecialchars($_POST['Name']);
+$visitor_email = filter_var($_POST['Email'], FILTER_VALIDATE_EMAIL);
+$number = htmlspecialchars($_POST['Number']);
+$location = htmlspecialchars($_POST['Location']);
+$message = htmlspecialchars($_POST['Message']);
 
-$email_form = 'info@yourwebsite.com';
+// Validate email
+if (!$visitor_email) {
+    die("Invalid email address.");
+}
 
+// Email details
+$email_from = 'khadhijaoffical@gmail.com'; // Use a valid domain email
 $email_subject = 'New Form Submission';
+$email_body = "User Name: $name\n" .
+              "User Email: $visitor_email\n" .
+              "User Number: $number\n" .
+              "User Location: $location\n" .
+              "Message: $message\n";
 
-$email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                "Subject: $subject.\n".
-                "User Message: $message ./n";
-
-$to = 'fudail.zafar@gmail.com';
-
+$to = 'mail4roughuse@gmail.com';
 $headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
-$headers .= "Reply-To: visitor_email \r\n";
-
-
-mail($to, $email_subject, $email_body, $headers);
-
-header("Location: contact.html");
+// Send email
+if (mail($to, $email_subject, $email_body, $headers)) {
+    header("Location: contact.html"); // Redirect on success
+    exit();
+} else {
+    echo "Error sending email. Please try again later.";
+}
 ?>
